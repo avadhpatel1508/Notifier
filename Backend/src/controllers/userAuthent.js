76ab761d4a9ff008a2815622dabeb18980d03d4e
avadhpatel1508 <work.avadhn@gmail.com>
2026-07-23 21:32:03 +0530

    auth and foundation phase complete

--- Backend/src/controllers/userAuthent.js
+++ Backend/src/controllers/userAuthent.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require('../models/User');
 const validate = require('../utils/validate');
+const userMiddleware = require('../middleware/userMiddleware');
 const register = async(req, res)=>{
     try {
         const{ name, emailId, password}=req.body;
@@ -31,5 +32,60 @@ const register = async(req, res)=>{
         res.status(500).json({ success: false, message: err.message,});
     }
 }
+const login  = async(req, res)=>{
+    try{
+        const {emailId, password}= req.body;
+        const isExist = await User.findOne({emailId});
+        if(!isExist) return res.status(409).json({ success: false, message: "Email not registered"});
 
-module.exports = {register};
+       
+        const user =await  User.findOne({emailId});
+        const isMatch = await bcrypt.compare(password, user.password);
+         if(!isMatch){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials",
+            });
+        }
+        const token = jwt.sign( { id: user._id, emailId: user.emailId }, process.env.JWT_SECRET, { expiresIn: "7d" } );
+
+        return res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            token,
+            data: {
+                id: user._id,
+                name: user.name,
+                emailId: user.emailId,
+            },
+        });
+
+    }
+    catch(err){
+        res.status(500).json({success: false, message: err.message});
+    }
+}
+
+const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+};
+module.exports = {register, login, getMe};
--- Backend/src/router/userAuth.js
+++ Backend/src/router/userAuth.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const {register} = require('../controllers/userAuthent');
+const {register, login, getMe} = require('../controllers/userAuthent');
+const userMiddleware = require('../middleware/userMiddleware');
+
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.get("/getMe", userMiddleware, getMe);
+
+
 
 module.exports = authRouter;
